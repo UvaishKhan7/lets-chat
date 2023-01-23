87cd7ee211542ddacd27dd213728999147f0a68e
Uvaish Khan <uvaish8248@gmail.com>
2023-01-23 12:08:08 +0530

    username component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/home/Home.js';
+import Signin from './components/signin/Signin.js';
+import Signup from './components/signup/Signup.js';
+import Profile from './components/profile/Profile.js';
+import Username from './components/username/Username.js';
+import PageNotFound from './components/pageNotFound/PageNotFound.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello the react app</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/username' element={<Username />} />
+      <Route path='*' element={<PageNotFound />} />
+    </Routes>
   );
 }
 
